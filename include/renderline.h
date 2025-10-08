d4681cde3976e817bb8e845ba5f37b95b431a7d3
bomba-cat <bledion.morina@hotmail.com>
2025-10-08 17:21:02 +0200

    Forgot to correct RL_SetGameInfo(); inside the include

--- include/renderline.h
+++ include/renderline.h
@@ -9,7 +9,7 @@ typedef enum {
 } RL_Error;
 
 RL_Error RL_Init();
-RL_Error RL_SetGameInfoStruct(RL_GameInfo gameinfo);
+RL_Error RL_SetGameInfo(RL_GameInfo gameinfo);
 RL_Error RL_SetGameInfoParams(char *name, char *descprtion, char *version);
 RL_Error RL_GetGameInfo(RL_GameInfo *gameinfo);
 RL_Error RL_Quit();
